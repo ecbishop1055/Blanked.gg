5ea9b9d1f9df63810628b3e3d686c2f519442dc6
ecbishop1055 <ethancbishop1@gmail.com>
2021-06-07 15:13:39 -0500

    Initial Commit

---
+++ src/main/java/com/example/Blanked/BlankedApplication.java
@@ -0,0 +1,13 @@
+package com.example.Blanked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlankedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlankedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Blanked/BlankedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Blanked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlankedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
